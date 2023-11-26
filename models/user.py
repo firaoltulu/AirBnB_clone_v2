ac5f4912528b85de2b799fc4f6ae1e40e99ba235
unknown <firaoltulu5@gmail.com>
2023-11-26 03:11:05 +0300

    my seventheen commit

--- models/user.py
+++ models/user.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python3
-"""This module defines a class User"""
-from models.base_model import BaseModel
+"""Defines the User class."""
 from models.base_model import Base
+from models.base_model import BaseModel
 from sqlalchemy import Column
 from sqlalchemy import String
 from sqlalchemy.orm import relationship
 
 
 class User(BaseModel, Base):
-    """This class defines a user by various attributes"""
-
     """Represents a user for a MySQL database.
 
     Inherits from SQLAlchemy Base and links to the MySQL table users.
@@ -23,6 +21,7 @@ class User(BaseModel, Base):
         places (sqlalchemy relationship): The User-Place relationship.
         reviews (sqlalchemy relationship): The User-Review relationship.
     """
+
     __tablename__ = "users"
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
